56239f4c1980125d58b6d9a2438cdb6722e3dfcc
Leonard <inf18193@lehre.dhbw-stuttgart.de>
2020-06-05 18:10:04 +0200

    game

--- game.py
+++ game.py
@@ -1,1 +1,151 @@
-print("hello World")
+import superior.py
+
+
+# global constants
+player1 = "X"
+player2 = "O"
+EMPTY = " "
+TIE = "TIE"
+NUM_SQUARES = 9
+
+
+
+
+def ask_yes_no(question):
+    """Ask a yes or no question."""
+    response = None
+    while response not in ("y", "n"):
+        response = input(question).lower()
+    return response
+
+
+def ask_number(question, low, high):
+    """Ask for a number within a range."""
+    response = None
+    while response not in range(low,high):
+        response = int(input(question))
+    return response
+
+
+def new_board():
+    """Create new game board."""
+    board = []
+    for square in range(NUM_SQUARES):
+        board.append(EMPTY)
+    return board
+
+
+def display_board(board):
+    """Display game board on screen."""
+    print("\n\t", board[0], "|", board[1], "|", board[2])
+    print("\t", "---------")
+    print("\n\t", board[3], "|", board[4], "|", board[5])
+    print("\t", "---------")
+    print("\n\t", board[6], "|", board[7], "|", board[8], "\n")
+
+
+def legal_moves(board):
+    """Create list of legal moves."""
+    moves = []
+    for square in range(NUM_SQUARES):
+        if board[square] == EMPTY:
+            moves.append(square)
+    return moves
+
+
+def winner(board):
+    """Determine the game winner."""
+    WAYS_TO_WIN = ((0, 1, 2),
+                   (3, 4, 5),
+                   (6, 7, 8),
+                   (0, 3, 6),
+                   (1, 4, 7),
+                   (2, 5, 8),
+                   (0, 4, 8),
+                   (2, 4, 6))
+    for row in WAYS_TO_WIN:
+        if board[row[0]] == board[row[1]] == board[row[2]] != EMPTY:
+            winner = board[row[0]]
+            return winner
+    if EMPTY not in board:
+        return TIE
+    return None
+
+
+def human_move(board, human):
+    """Get human move."""
+    legal = legal_moves(board)
+    move = None
+    while move not in legal:
+        move = ask_number("Where will you move? (0-8): ", 0, NUM_SQUARES)
+        if move not in legal:
+            print("\nThat square is already occupied, foolish human. Choose another.\n")
+    print("Fine...")
+    return move
+
+
+def perfect_moves(board, computer, human):
+    """Make computer move."""
+
+    # make a copy to work with since function will be changing list
+    board = board[:]
+
+    # the best positions to have, in order
+    BEST_MOVES = (4, 0, 2, 6, 8, 1, 3, 5, 7)
+
+    print("I shall take square number", end=" ")
+
+    # if computer can win, take that move
+    for move in legal_moves(board):
+        board[move] = computer
+        if winner(board) == computer:
+            print(move)
+            return move 
+        # done checking this move, undo it
+        board[move] = EMPTY
+
+    # if human can win, block that move
+    for move in legal_moves(board):
+        board[move] = human
+        if winner(board) == human:
+            print(move)
+            return move
+        # done checking this move, undo it
+        board[move] = EMPTY
+
+    # since no one can win on next move, pick best open square
+    for move in BEST_MOVES:
+        if move in legal_moves(board):
+            print(move)
+            return move
+
+
+def next_turn(turn):
+    """Switch turns."""
+    if turn == player1:
+        return player2
+    else:
+        return player1
+
+        
+def main():
+    turn = player1
+    board = new_board()
+    display_board(board)
+
+    while not winner(board):
+        if turn == player2:
+            move = human_move(board, player2)
+            board[move] = player2
+        else:
+            move = ai_move(board, player1)
+            board[move] = player1
+        display_board(board)
+        turn = next_turn(turn)
+
+    the_winner = winner(board)
+    print("winner: ", the_winner)
+
+# start the program
+main()
+input("\n\nPress the enter key to quit.")
